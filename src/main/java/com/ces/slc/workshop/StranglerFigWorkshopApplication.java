0d0ae268f1025e786d69dff20e92022760265ccf
Guus Lieben <glieben@costengineering.eu>
2024-04-24 14:22:40 +0200

    SLC-268 Set up template for workshop project

---
+++ src/main/java/com/ces/slc/workshop/StranglerFigWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.ces.slc.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StranglerFigWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StranglerFigWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ces/slc/workshop/StranglerFigWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ces.slc.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StranglerFigWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
